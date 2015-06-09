2b2526b497489f36f8639b326c77b8ac9bc1dc8f
Edi Carlos Simões de Oliveira <edicarlosma@gmail.com>
2015-06-09 09:40:25 -0300

    Pegar Endreço MAC

--- SysClinic/SsysClinic.View/Program.cs
+++ SysClinic/SsysClinic.View/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SsysClinic.View.Mdi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace SysClinic.View
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MdiPrincipal());
+            Application.Run(new frmValidacao());
         }
     }
 }
